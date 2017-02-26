85a7507b84a900191a88dfaf2af2851d58c98608
Valerie McCulloch <valeriemcculloch@Valeries-MacBook-Pro.local>
2017-02-26 01:17:13 -0500

    added hints

--- app/src/main/java/com/hacksmith/vmcculloch/crave/ProfileActivity.java
+++ app/src/main/java/com/hacksmith/vmcculloch/crave/ProfileActivity.java
@@ -2,12 +2,20 @@ package com.hacksmith.vmcculloch.crave;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class ProfileActivity extends AppCompatActivity {
+    private EditText editText;
+    private String editString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+
     }
+
+
 }
