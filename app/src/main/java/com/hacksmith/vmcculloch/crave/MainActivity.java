c0e10d63cf3922436808098e2d944ce278115854
Valerie McCulloch <valeriemcculloch@Valeries-MacBook-Pro.local>
2017-02-26 01:36:35 -0500

    password font

--- app/src/main/java/com/hacksmith/vmcculloch/crave/MainActivity.java
+++ app/src/main/java/com/hacksmith/vmcculloch/crave/MainActivity.java
@@ -1,17 +1,22 @@
 package com.hacksmith.vmcculloch.crave;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText eText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        eText = (EditText) findViewById(R.id.playerpassword) ;
+        eText.setTypeface(Typeface.DEFAULT);
 
         ((Button) findViewById(R.id.loginButton)).setOnClickListener(new View.OnClickListener(){
             @Override
